cbc8f02f907f7b77480bb0f885b7a880899bdd1e
Kento Nakayama <kento427@outlook.com>
2024-04-05 22:12:32 +0900

    Refactor installation script for IIS in installiis.ps1

--- Blog/vm_customscript/installiis.ps1
+++ Blog/vm_customscript/installiis.ps1
@@ -1,3 +1,1 @@
 Install-WindowsFeature -Name Web-Server -IncludeManagementTools
-Set-Location -Path C:\inetpub\wwwroot
-Add-Content iisstart.htm "<H1><center>WELCOME to my Web Server $env:COMPUTERNAME, Azure Network Rocks!!!!!!!</center></H1>"
